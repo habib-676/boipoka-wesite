94a9d42c39892b9f17cb8e0a026cd7c0d23bd91a
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-20 19:42:57 +0600

    banner done

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root/Root";
+import ErrorPage from "../pages/error-page/ErrorPage";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World </div>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index : true,
+        path: "/",
+        Component: Home,
+      },
+    ],
   },
 ]);
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import bookImg from "../../assets/books.jpg";
+import Books from "../Books/Books";
+
+const Home = () => {
+  return (
+    <div>
+      {/* banner */}
+      <div className="hero bg-base-200 p-16 my-10">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={bookImg} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">
+              Books to freshen up <br />
+              your bookshelf
+            </h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary bg-green-700">
+              View the list
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <Books></Books>
+    </div>
+  );
+};
+
+export default Home;
